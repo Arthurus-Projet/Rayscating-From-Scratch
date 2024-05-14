c2e8abbb1dd9c6aae36e7045f92673ee5b31149f
Arthurus-Projet <133526137+Arthurus-Projet@users.noreply.github.com>
2024-05-15 01:01:45 +0200

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,52 @@
+// player.cpp
+
+#include "Headers/Player.h"
+
+// Constructeur par défaut
+Player::Player() : x(0.0), y(0.0), speed(0.0), angle(0.0), health(100) {}
+
+// Constructeur surchargé
+Player::Player(double x, double y, double speed, double angle, int health)
+    : x(x), y(y), speed(speed), angle(angle), health(health) {}
+
+// Méthodes pour accéder aux membres privés
+double Player::getX() const {
+    return x;
+}
+
+double Player::getY() const {
+    return y;
+}
+
+double Player::getSpeed() const {
+    return speed;
+}
+
+double Player::getAngle() const {
+    return angle;
+}
+
+int Player::getHealth() const {
+    return health;
+}
+
+// Méthodes pour modifier les membres privés
+void Player::setX(double newX) {
+    x = newX;
+}
+
+void Player::setY(double newY) {
+    y = newY;
+}
+
+void Player::setSpeed(double newSpeed) {
+    speed = newSpeed;
+}
+
+void Player::setAngle(double newAngle) {
+    angle = newAngle;
+}
+
+void Player::setHealth(int newHealth) {
+    health = newHealth;
+}
