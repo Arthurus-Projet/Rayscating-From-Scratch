73de7dc9a508a5bfb7fc2d7619312cb4ba210309
Arthurus-Projet <133526137+Arthurus-Projet@users.noreply.github.com>
2024-05-15 17:31:47 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ int main() {
     double size = 4;
     double windowHeight = 800;
     double windowWidth = 500;
-    double speed = 3;
-    int angle = 90;
+    double speed = 0.01;
+    int reference_angle = 90;
     double health = 10;
-    Player player = Player(playerX, playerY, speed, angle, health);
+    Player player = Player(playerX, playerY, speed, reference_angle, health);
     Collision collision = Collision(player);
     
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML Raycasting");
@@ -51,51 +51,52 @@ int main() {
         float speed = 5.0f;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
             // to move forward
+            collision.player.setX(collision.player.getX() + collision.player.getSpeed() * std::cos(degToRad(collision.player.getAngle())));
+            collision.player.setY(collision.player.getY() - collision.player.getSpeed() * std::sin(degToRad(collision.player.getAngle())));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
             // to move backwards
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            referenceAngle--;
-            if (referenceAngle == -1)
-                referenceAngle = 359;
+            collision.player.setAngle(collision.player.getAngle() - 1);
+            if (collision.player.getAngle() == -1)
+                collision.player.setAngle(359);
             
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-            referenceAngle++;
-            if (referenceAngle == 360)
-                referenceAngle = 0;
+            collision.player.setAngle(collision.player.getAngle() + 1);
+            if (collision.player.getAngle() == 360)
+                collision.player.setAngle(0);
         }
 
         window.clear();
         int i = 0;
-        for(r = referenceAngle + 30.0; r >= referenceAngle -31.0; r-= 1.0) {
+        for(r = collision.player.getAngle() + 30.0; r >= collision.player.getAngle() -31.0; r-= 1.0) {
             rayon = modulo(r, 360);
             std::pair<double, double> pos;
-            collision.player.setAngle(rayon);
+            
 
             if (0.0 <= rayon == true && rayon < 90.0 == true) {
-                pos = collision.collision_0_90(map);
-                std::cout << pos.first << std::endl;
+                pos = collision.collision_0_90(map, rayon);
             }
 
             if (90 <= rayon == true && rayon < 180 == true) {
-                pos = collision.collision_90_180(map);
+                pos = collision.collision_90_180(map, rayon);
             }
 
             if (270 < rayon == true && rayon < 360 == true) {
-                pos = collision.collision_270_360(map);
+                pos = collision.collision_270_360(map, rayon);
             }
 
             if (180 <= rayon == true && rayon <= 270 == true) {
-                pos = collision.collision_180_270(map);
+                pos = collision.collision_180_270(map, rayon);
             }
 
-            double sideX = 4.4 - pos.first;
-            double sideY = 6.2 - pos.second;
+            double sideX = collision.player.getX() - pos.first;
+            double sideY = collision.player.getY() - pos.second;
             double hyp = std::sqrt(sideX * sideX + sideY * sideY);
 
-            double differenceWithPlayer = std::abs(rayon - referenceAngle);
+            double differenceWithPlayer = std::abs(rayon - collision.player.getAngle());
 
             // fish eye correction
             double d = hyp * std::cos(degToRad(differenceWithPlayer));
